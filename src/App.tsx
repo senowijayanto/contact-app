5f88b61edcdf0f1a03ef944dd4f849ac4b6165b7
Seno Wijayanto <senowijayanto@gmail.com>
2024-12-03 09:13:11 +0700

    Addr render list from contact items

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,30 @@ import React from 'react';
 import Contact from "./components/Contact";
 
 function App() {
-  return (
-    <div>
-      <Contact name="Bryan Curry" phone="082235436577" />
-      <Contact name="Steve Cox" phone="081546978608" />
-      <Contact name="Mark Alexanderson" phone="085876980707" />
-    </div>
-  );
+    const contactItems = [
+        {
+            id: 1,
+            name: "Bryan Curry",
+            phone: "082235436577"
+        },
+        {
+            id: 2,
+            name: "Steve Cox",
+            phone: "081546978608"
+        },
+        {
+            id: 3,
+            name: "Mark Alexander",
+            phone: "085876980707"
+        }
+    ]
+    return (
+        <div>
+            {contactItems.map((item, index) => (
+                <Contact name={item.name} phone={item.phone} key={index} />
+            ))}
+        </div>
+    );
 }
 
 export default App;
